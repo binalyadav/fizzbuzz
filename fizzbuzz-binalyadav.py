2cec1d0fad6682d9b327f6537aa7f2ba412cbd53
binalyadav <B.Yadav@dal.ca>
2019-10-09 10:09:09 -0700

    fizzbuzz implementation

---
+++ fizzbuzz-binalyadav.py
@@ -0,0 +1,16 @@
+for fizzbuzz in range(100):  
+  
+    
+ 
+    
+    if fizzbuzz % 3 == 0:      
+        print("Fizz")                                          
+        continue
+  
+    
+    elif fizzbuzz % 5 == 0:          
+        print("Buzz")                                      
+        continue
+  
+    
+    print(fizzbuzz) 
